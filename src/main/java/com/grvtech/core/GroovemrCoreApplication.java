a7cb119cbc45c300639806e80e7275d12799fb6c
Radu Gabor <radu@grvtech.ca>
2018-07-24 08:02:42 -0400

    first commit

---
+++ src/main/java/com/grvtech/core/GroovemrCoreApplication.java
@@ -0,0 +1,12 @@
+package com.grvtech.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroovemrCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GroovemrCoreApplication.class, args);
+	}
+}
