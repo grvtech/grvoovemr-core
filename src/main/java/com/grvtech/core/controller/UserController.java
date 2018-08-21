21dac67fa9c9090d3b84c4af910a25126ec4b33e
Radu Gabor <radu@grvtech.ca>
2018-08-21 14:15:32 -0400

    21 -August-2018

--- src/main/java/com/grvtech/core/controller/UserController.java
+++ src/main/java/com/grvtech/core/controller/UserController.java
@@ -1,5 +1,25 @@
 package com.grvtech.core.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.grvtech.core.model.User;
+import com.grvtech.core.service.IUserService;
+
+@RestController
 public class UserController {
+
+	@Autowired
+	private IUserService userservice;
+
+	@RequestMapping("/user/{userId}")
+	public ResponseEntity<User> getUser(@PathVariable int userId) {
+		User u1 = userservice.getUserById(userId);
+		return new ResponseEntity<User>(u1, HttpStatus.OK);
+	}
 
 }
